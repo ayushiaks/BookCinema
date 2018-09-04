27845c412a1780ed52b3d84175ceac2d300a8b30
ayushiaks <ayushiaks8@gmail.com>
2018-09-04 16:25:25 +0530

    Uploaded

---
+++ app/src/main/java/com/example/project/BookCinema/lastPage.java
@@ -0,0 +1,13 @@
+package com.example.chinmaymahajan.bookmyshow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class lastPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_last_page);
+    }
+}
